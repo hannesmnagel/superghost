f8b5631af7e2f4fa45dc08507d244b4fed7c9613
Hannes Nagel <hannes.nagel@codeunity.de>
2024-06-15 19:39:22 +0200

    Initial Commit

---
+++ superghost/superghostApp.swift
@@ -0,0 +1,17 @@
+//
+//  superghostApp.swift
+//  superghost
+//
+//  Created by Hannes Nagel on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct superghostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
