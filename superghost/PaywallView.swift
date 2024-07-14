3e1b4cf52e39d37c6b6c7ff2410a57c21ee90db7
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-14 18:13:18 +0200

    added paywallview

--- superghost/PaywallView.swift
+++ superghost/PaywallView.swift
@@ -6,10 +6,38 @@
 //
 
 import SwiftUI
+import RevenueCat
+import RevenueCatUI
 
 struct PaywallView: View {
+    @State private var products : Result<Offerings,(any Error)>? = nil
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            if let products{
+                switch products {
+                case .success(let offering):
+                    if let offering = offering.current {
+                        RevenueCatUI.PaywallView(offering: offering, displayCloseButton: true)
+                            .onRequestedDismissal {
+                                dismiss()
+                            }
+                    } else {
+                        ContentUnavailableView("There is nothing available to purchase", systemImage: "questionmark.folder", description: Text("No products found"))
+                    }
+                case .failure(_):
+                    ContentUnavailableView("You can't upgrade right now", systemImage: "network.slash", description: Text("An error occured"))
+                }
+            }
+        }
+            .task {
+                do{
+                    products = try await .success(Purchases.shared.offerings())
+                } catch{
+                    products = .failure(error)
+                }
+            }
     }
 }
 
