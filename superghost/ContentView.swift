3041e61deac3953a43423a57f6315172c66dcad9
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-14 17:33:16 +0200

    refactored and added firstuseview

--- superghost/ContentView.swift
+++ superghost/ContentView.swift
@@ -6,268 +6,74 @@
 //
 
 import SwiftUI
+import RevenueCat
+
+struct BlackOutTransition: Transition {
+    func body(content: Content, phase: TransitionPhase) -> some View {
+        content
+            .blur(radius: phase == .identity ? 0 : 10)
+            .overlay{
+                if !(phase == .identity){
+                    Rectangle().fill(Color.black).ignoresSafeArea()
+                }
+            }
+    }
+}
 
 struct ContentView: View {
-
+    @AppStorage("isFirstUse") var isFirstUse = true
     @State var isGameViewPresented = false
-
     @StateObject var viewModel = GameViewModel()
-
-    let date = Date()
-    @Namespace var namespace
-
-    @State var joinGame = false
-
-    @State private var gameStatSelection: GameStat?
-
-    @Query(sort: [SortDescriptor(\GameStat.createdAt, order: .reverse)]) var games : [GameStat]
+    @State private var showTrialEndsIn : Int?
+    @State private var isSuperghost = false
 
     var body: some View {
         Group{
-            if isGameViewPresented{
-                GameView(viewModel: viewModel, isPresented: $isGameViewPresented, namespace: namespace)
+            if isFirstUse{
+                FirstUseView()
+                    .transition(BlackOutTransition())
+            } else if isGameViewPresented{
+                GameView(viewModel: viewModel, isPresented: $isGameViewPresented)
+                    .transition(BlackOutTransition())
             } else {
-                GeometryReader{geo in
-                    VStack {
-                        List{
-                            Section{
-                                StatsView(selection: $gameStatSelection)
-                            } header: {
-                                VStack{
-                                    waitingGhost
-
-                                    AsyncButton {
-                                        try await viewModel.getTheGame()
-                                        isGameViewPresented = true
-                                    } label: {
-                                        Text("Start")
-                                    }
-                                    .disabled(games.today.lost.count == 5)
-                                    #if !os(watchOS)
-                                    .keyboardShortcut(.defaultAction)
-                                    #endif
-
-                                    AsyncButton {
-                                        try await viewModel.hostGame()
-                                        isGameViewPresented = true
-                                    } label: {
-                                        Text("Host a Game")
-                                    }
-                                }
-                                .font(ApearanceManager.title)
-                                .buttonStyle(.bordered)
-                                .tint(.accent)
-                                .frame(maxWidth: .infinity)
-                                .frame(height: geo.size.height*0.7, alignment: .bottom)
-                                .padding(.bottom, 30)
-                            }
-                        }
-                        #if os(macOS)
-                        .listStyle(.sidebar)
-                        #endif
-                        .scrollContentBackground(.hidden)
-                    }
-                    .onOpenURL { url in
-                        Task{
-                            guard url.scheme == "superghost" else {
-                                return
-                            }
-                            let gameId = url.lastPathComponent
-
-                            try await viewModel.joinGame(with: gameId)
-                            gameStatSelection = nil
-                            isGameViewPresented = true
-                        }
-                    }
-                }
+                HomeView(isSuperghost: isSuperghost, viewModel: viewModel, isGameViewPresented: $isGameViewPresented)
+                    .transition(BlackOutTransition())
             }
         }
+        .animation(.smooth, value: isFirstUse)
         .animation(.smooth, value: isGameViewPresented)
-        .sheet(item: $gameStatSelection) { gameStat in
-            VStack{
-                WordDefinitionView(word: gameStat.word, game: gameStat)
-                    .padding(.top)
-                #if !os(watchOS)
-                    .overlay(alignment: .topTrailing){
-                        Button{gameStatSelection = nil} label: {
-                            Image(systemName: "xmark")
-                        }
-                        .padding(.top.union(.trailing))
-                    }
-                #endif
-            }
-            .background((gameStat.won ? Color.green.brightness(0.5).opacity(0.1) : Color.red.brightness(0.5).opacity(0.1)).ignoresSafeArea())
-            #if os(macOS)
-            .frame(minWidth: 500, minHeight: 500)
-            #endif
-        }
         .preferredColorScheme(.dark)
+        .task {
+            do{
+                try await fetchSubscription()
+            } catch {
+                print(error)
+            }
+        }
+        .sheet(isPresented: $viewModel.showPaywall) {
+            Task{try? await fetchSubscription()}
+        } content: {
+            PaywallView()
+        }
+        .fontDesign(.rounded)
     }
 
-    @MainActor @ViewBuilder
-    var waitingGhost: some View {
-        TimelineView(.animation){context in
+    func fetchSubscription() async throws {
+        let info = try await Purchases.shared.customerInfo()
+        let subscriptions = info.activeSubscriptions
+        let timeSinceTrialEnd = (Calendar.current.date(byAdding: .day, value: -7, to: .now) ?? .now).timeIntervalSince(info.firstSeen)
+        let daysSinceTrialEnd = timeSinceTrialEnd / (Calendar.current.dateInterval(of: .day, for: .now)?.duration ?? 1)
+        isSuperghost = subscriptions.contains("monthly.superghost") || timeSinceTrialEnd < 0
 
-            let timeInterval = context.date.timeIntervalSince(date)
-            let sineValue = sin(timeInterval * .pi / 2) // Adjust the frequency of the sine wave
-            let cosineValue = cos(timeInterval * .pi / 2)
-
-            let offsetX = CGFloat(sineValue)*30
-            let rotationAngle = atan2(cosineValue, 1.0) * 20 / .pi
-
-            Image(.ghost)
-                .resizable()
-                .scaledToFit()
-                .visualEffect { content, geo in
-                    content
-                        .offset(x: offsetX)
-                        .rotationEffect(.degrees(rotationAngle))
-                        .offset(y: -geo.frame(in: .scrollView).minY*0.5 + geo.size.height/2)
-                        .scaleEffect(1+geo.frame(in: .scrollView).minY/1000)
-                }
-                .matchedGeometryEffect(id: "ghost", in: namespace)
-                .padding(.vertical, 60)
-                .padding(.bottom, 60)
+        //is in trial:
+        if !subscriptions.contains("monthly.superghost") && timeSinceTrialEnd < 0 {
+            showTrialEndsIn = Int(-daysSinceTrialEnd)
+        }
+        //is not superghost, every 4 days:
+        if !isSuperghost && (Int(daysSinceTrialEnd) % 4 == 0 || daysSinceTrialEnd < 3) {
+            viewModel.showPaywall = true
         }
     }
-}
-
-import SwiftData
-
-struct StatsView: View {
-    @Query(sort: [SortDescriptor(\GameStat.createdAt, order: .reverse)]) var games : [GameStat]
-
-    @Binding var selection: GameStat?
-
-    var body: some View {
-        #if os(macOS)
-        VStack{}
-            .toolbar {
-                summary
-            }
-        #else
-        summary
-        #endif
-        ForEach(games){game in
-            Button{selection = game} label: {
-                HStack{
-                    Text(game.word)
-                    Spacer()
-                    Image(systemName: game.won ? "crown.fill" : "xmark")
-                }
-                .contentShape(.rect)
-            }
-            .buttonStyle(.plain)
-            .listRowBackground(game.won ? Color.green.brightness(0.5).opacity(0.1) : Color.red.brightness(0.5).opacity(0.1))
-        }
-    }
-    @ViewBuilder @MainActor
-    var summary: some View {
-        let winningRate = games.winningRate
-        let winningStreak = games.winningStreak
-        let gamesToday = games.today
-        let wonToday = gamesToday.won.count
-        let gamesLostToday = gamesToday.lost
-        let wordToday = "GHOST".prefix(gamesLostToday.count).appending("-----".prefix(5-gamesLostToday.count))
-        HStack(alignment: .top){
-#if os(macOS)
-            VStack{
-                Text(wordToday)
-                Text("word today")
-                    .font(ApearanceManager.footnote)
-            }
-            .frame(maxWidth: .infinity)
-            Divider()
-#endif
-            VStack{
-                Text(winningStreak, format: .number)
-                Text("Winning Streak")
-                    .font(ApearanceManager.footnote)
-            }
-            .frame(maxWidth: .infinity)
-            Divider()
-            VStack{
-                Text(winningRate, format: .percent.precision(.fractionLength(0)))
-                Text("Winning Rate")
-                    .font(ApearanceManager.footnote)
-            }
-            .frame(maxWidth: .infinity)
-            Divider()
-            VStack{
-                Text(wonToday, format: .number)
-                Text("Wins Today")
-                    .font(ApearanceManager.footnote)
-            }
-            .frame(maxWidth: .infinity)
-        }
-        #if os(watchOS)
-        .font(ApearanceManager.headline)
-        #else
-        .font(ApearanceManager.title)
-        #endif
-        .multilineTextAlignment(.center)
-        .listRowBackground(
-            HStack{
-                GeometryReader{geo in
-#if os(watchOS)
-                    UnevenRoundedRectangle(topLeadingRadius: 10, bottomLeadingRadius: 10)
-                        .fill(.red.opacity(0.5 + 0.1 * Double(gamesLostToday.count)))
-                        .frame(width:
-                                geo.frame(in: .named("rowbackground")).width * CGFloat(gamesLostToday.count) / 5.0
-                        )
-#else
-                    Rectangle()
-                        .fill(.red.opacity(0.5 + 0.1 * Double(gamesLostToday.count)))
-                        .frame(width:
-                                geo.frame(in: .named("rowbackground")).width * CGFloat(gamesLostToday.count) / 5.0
-                        )
-#endif
-                }
-            }
-                .coordinateSpace(name: "rowbackground")
-                .ignoresSafeArea()
-                .background(Color.green.opacity(0.5))
-        )
-        .clipped()
-
-#if !os(macOS)
-        LabeledContent("Today", value: wordToday)
-            .listRowBackground(
-                HStack{
-                    GeometryReader{geo in
-                        #if os(watchOS)
-                        UnevenRoundedRectangle(topLeadingRadius: 10, bottomLeadingRadius: 10)
-                            .fill(.red.opacity(0.5 + 0.1 * Double(gamesLostToday.count)))
-                            .frame(width:
-                                    geo.frame(in: .named("rowbackground")).width * CGFloat(gamesLostToday.count) / 5.0
-                            )
-                        #else
-                        Rectangle()
-                            .fill(.red.opacity(0.5 + 0.1 * Double(gamesLostToday.count)))
-                            .frame(width:
-                                    geo.frame(in: .named("rowbackground")).width * CGFloat(gamesLostToday.count) / 5.0
-                            )
-                        #endif
-                    }
-                }
-                    .coordinateSpace(name: "rowbackground")
-                    .ignoresSafeArea()
-                    .background(Color.green.opacity(0.5))
-            )
-#endif
-    }
-}
-
-extension Array where Element == GameStat {
-    var won: Self {filter{$0.won}}
-    var lost: Self {filter{!$0.won}}
-    var winningRate: Double {
-        isEmpty ? 0.0 : Double(won.count)/Double(count)
-    }
-    var winningStreak : Int { sorted{$0.createdAt > $1.createdAt}.firstIndex(where: {!$0.won}) ?? count}
-    var today: Self {filter{Calendar.current.isDateInToday($0.createdAt)}}
-    var withInvitation: Self {filter{$0.withInvitation}}
-
 }
 
 #Preview {
@@ -275,47 +81,10 @@ extension Array where Element == GameStat {
         .modifier(PreviewModifier())
 }
 
-struct AsyncButton<Label:View>: View {
+import TipKit
 
-    let action: () async throws -> Void
-    @ViewBuilder let label: Label
-
-    @State private var state = AsyncButtonState.main
-
-    enum AsyncButtonState{
-        case main, inProgress, success, failed
-    }
-
-    var body: some View {
-        Button{
-            Task{
-                do{
-                    state = .inProgress
-                    try await action()
-                    state = .success
-                    try? await Task.sleep(for: .seconds(1))
-                    state = .main
-                } catch {
-                    print(error)
-                    state = .failed
-                    try? await Task.sleep(for: .seconds(1))
-                    state = .main
-                }
-            }
-        } label: {
-            switch state {
-            case .main:
-                label
-            case .inProgress:
-                ProgressView()
-            case .success:
-                Image(systemName: "checkmark")
-                    .foregroundStyle(.green)
-            case .failed:
-                Image(systemName: "xmark")
-                    .foregroundStyle(.red)
-            }
-        }
-        .disabled(state == .inProgress)
-    }
+struct HowToPlayTip: Tip {
+    var title = Text("Get Started")
+    var message = Text("Learn how to play and view tips to win!")
+    var actions : [Tip.Action] { [Tip.Action(id: "learn-how-to-play", title: "Learn more")] }
 }
