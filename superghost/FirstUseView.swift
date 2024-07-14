be3d4b759d62e9f3789d8b1bb1cf098235eb23c3
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-14 17:33:29 +0200

    added firstuseview

--- superghost/FirstUseView.swift
+++ superghost/FirstUseView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct FirstUseView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State var firstUseState = FirstUseState.tapToContinue
+
+    enum FirstUseState: CaseIterable{
+        case tapToContinue, howTo, end
     }
+
+    var body: some View {
+        TabView(selection: $firstUseState){
+            VStack{
+                Image(.ghostHeadingLeft)
+                    .resizable()
+                    .scaledToFit()
+                Text("Superghost")
+                    .font(ApearanceManager.largeTitle)
+                    .fontWeight(.heavy)
+                Text("Tap to play")
+                    .font(ApearanceManager.headline)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onTapGesture {
+                firstUseState.nextCase()
+            }
+            .tag(FirstUseState.tapToContinue)
+            .tabItem {}
+            InstructionsView{firstUseState.nextCase()}
+                .tag(FirstUseState.howTo)
+                .tabItem {}
+            Text("Let's go")
+                .font(ApearanceManager.largeTitle.bold())
+                .task{
+                    try? await Task.sleep(for: .milliseconds(500))
+                    finished()
+                }
+                .tag(FirstUseState.end)
+                .tabItem {}
+        }
+    }
+
+    func finished(){UserDefaults.standard.set(false, forKey: "isFirstUse")}
 }
 
 #Preview {
     FirstUseView()
+        .modifier(PreviewModifier())
+}
+
+extension CaseIterable where Self: Equatable {
+    mutating func nextCase() {
+        guard let currentIndex = Self.allCases.firstIndex(of: self) else { return }
+        let nextIndex = Self.allCases.index(after: currentIndex)
+        self = nextIndex == Self.allCases.endIndex ? Self.allCases.first! : Self.allCases[nextIndex]
+    }
 }
