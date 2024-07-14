cf6f551df55e194c9ccc29dfbf313df24ebd6888
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-14 17:33:40 +0200

    refactored

--- superghost/GameStat.swift
+++ superghost/GameStat.swift
@@ -25,3 +25,15 @@ class GameStat: Hashable, Identifiable {
         self.withInvitation = withInvitation
     }
 }
+
+extension Array where Element == GameStat {
+    var won: Self {filter{$0.won}}
+    var lost: Self {filter{!$0.won}}
+    var winningRate: Double {
+        isEmpty ? 0.0 : Double(won.count)/Double(count)
+    }
+    var winningStreak : Int { sorted{$0.createdAt > $1.createdAt}.firstIndex(where: {!$0.won}) ?? count}
+    var today: Self {filter{Calendar.current.isDateInToday($0.createdAt)}}
+    var withInvitation: Self {filter{$0.withInvitation}}
+
+}
