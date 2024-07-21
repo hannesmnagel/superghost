8cba7c4389a7f75cea8b9aaff4d26190344bfc0a
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-21 14:04:13 +0200

    apearances

--- superghost/Appearance.swift
+++ superghost/Appearance.swift
@@ -7,7 +7,23 @@
 
 import SwiftUI
 
+private enum OS{
+    case watchOS, iOS, macOS, visionOS
+}
+#if os(watchOS)
+private let os = OS.watchOS
+#elseif os(iOS)
+private let os = OS.iOS
+#elseif os(macOS)
+private let os = OS.macOS
+#elseif os(visionOS)
+private let os = OS.visionOS
+#endif
+
 class ApearanceManager {
+    static let hostGame: Font = os == .watchOS ? .headline : .largeTitle
+    static let startGame: Font = os == .watchOS ? .title : .largeTitle
+    static let howToPlayTitle: Font = os == .watchOS ? .title2.bold() : .largeTitle.bold()
     #if os(watchOS)
     static let largeTitle = Font.title
     #else
