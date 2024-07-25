e18405c69959422e1f621f08a79b647d835defbd
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-25 20:25:41 +0200

    watch app test

---
+++ superghost for watch Watch App/superghost_for_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  superghost_for_watchApp.swift
+//  superghost for watch Watch App
+//
+//  Created by Hannes Nagel on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct superghost_for_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
