56173fc891a5007377d5b1035f9b95a2313adb3e
Hannes Nagel <hannes.nagel@codeunity.de>
2024-07-26 11:44:00 +0200

    GameViewModel

--- superghost/superghostApp.swift
+++ superghost/superghostApp.swift
@@ -15,10 +15,15 @@ struct superghostApp: App {
         Purchases.logLevel = .error
         try! Purchases.configure(withAPIKey: String(contentsOf: Bundle.main.resourceURL!.appending(path: "revenuecatkey.txt")).trimmingCharacters(in: .whitespacesAndNewlines))
     }
+
+    @CloudStorage("isSuperghost") private var isSuperghost = false
+    @StateObject var viewModel = GameViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .modelContainer(for: GameStat.self)
+                .environmentObject(viewModel)
 #if os(macOS)
                 .frame(minHeight: 500)
 #endif
