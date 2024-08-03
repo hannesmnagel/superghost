9bf18515e9c2b635bf611b2858513ce9846c33b6
Hannes Nagel <hannes.nagel@codeunity.de>
2024-08-03 15:06:40 +0200

    reduced cpu usage

--- superghost/WaitingGhost.swift
+++ superghost/WaitingGhost.swift
@@ -11,26 +11,15 @@ struct WaitingGhost: View {
     let date = Date()
     
     var body: some View {
-        TimelineView(.animation){context in
 
-            let timeInterval = context.date.timeIntervalSince(date)
-            let sineValue = sin(timeInterval * .pi / 2) // Adjust the frequency of the sine wave
-            let cosineValue = cos(timeInterval * .pi / 2)
-
-            let offsetX = CGFloat(sineValue)*30
-            let rotationAngle = atan2(cosineValue, 1.0) * 20 / .pi
-
-
-            Video("waitingGhost")
-                .visualEffect { content, geo in
-                    content
-                        .offset(x: offsetX)
-                        .rotationEffect(.degrees(rotationAngle))
-                        .offset(y: -geo.frame(in: .scrollView).minY*0.8 + min(30, geo.size.height/2))
-                        .scaleEffect(0.8+geo.frame(in: .scrollView).minY/1000)
-                }
-        }
-        .notUpdating()
+        Video("waitingGhost")
+            .notUpdating()
+            .visualEffect { content, geo in
+                content
+                    .offset(y: -geo.frame(in: .scrollView).minY*0.8 + min(30, geo.size.height/2))
+                    .scaleEffect(0.8+geo.frame(in: .scrollView).minY/1000)
+            }
+            .notUpdating()
     }
 }
 
