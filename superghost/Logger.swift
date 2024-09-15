50a3d31f9e59010541b0b1af6db2fc5261ab35f2
Hannes Nagel <hannes.nagel@codeunity.de>
2024-09-15 21:14:51 +0200

    adding stats logs

--- superghost/Logger.swift
+++ superghost/Logger.swift
@@ -22,44 +22,78 @@ final class Logger {
     static let game = os.Logger(subsystem: subsystem, category: Category.game.rawValue)
     static let general = os.Logger(subsystem: subsystem, category: Category.general.rawValue)
     
-    struct Event: Codable {
-        let userId: String
-        let eventName: String
-        let timestamp: Date
-    }
     static func remoteLog(_ eventName: String) {
-        Task{
-            do {
-                let timeout = Date()
-                while !GKLocalPlayer.local.isAuthenticated,
-                      timeout < .now + 10 {
-                    try? await Task.sleep(for: .seconds(1))
+        analyzer.remoteLog(eventName)
+    }
+    
+    static func appDidActivate() {
+        analyzer.appDidActivate()
+    }
+    static func appDidDeactivate() {
+        analyzer.appDidDeactivate()
+    }
+    static private let analyzer = Analyzer()
+    
+    private final class Analyzer {
+        @CloudStorage("timeSpentInGhost") private var timeSpentInGhost = 0.0//in minutes
+        private var activated : Date?
+        
+        func appDidActivate() {
+            activated = .now
+        }
+        func appDidDeactivate() {
+            if let activated {
+                timeSpentInGhost += (Date.now.timeIntervalSince(activated)/60)
+            }
+            uploadStats()
+        }
+        
+        struct Event: Codable {
+            let userId: String
+            let eventName: String
+            let timestamp: Date
+        }
+        
+        func uploadStats(){
+#if !os(macOS)
+            remoteLog("stats|timeInGhost|\(Int(timeSpentInGhost))")
+#endif
+            remoteLog("stats|osVersion|\(ProcessInfo.processInfo.operatingSystemVersionString)")
+        }
+        func remoteLog(_ eventName: String){
+            Task{
+                do {
+                    let timeout = Date()
+                    while !GKLocalPlayer.local.isAuthenticated,
+                          timeout < .now + 10 {
+                        try? await Task.sleep(for: .seconds(1))
+                    }
+                    let event = Event(userId: GKLocalPlayer.local.gamePlayerID, eventName: eventName, timestamp: Date())
+                    let url = URL(string: "https://hannesnagel.com/api/v2/log")!
+                    
+                    // Convert Event to JSON data
+                    let encoder = JSONEncoder()
+                    print(encoder.dateEncodingStrategy)
+                    encoder.dateEncodingStrategy = .iso8601
+                    let jsonData = try encoder.encode(event)
+                    
+                    // Create the URLRequest
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "POST"
+                    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.httpBody = jsonData
+                    
+                    // Perform the request
+                    let (_, response) = try await URLSession.shared.data(for: request)
+                    
+                    // Check for a valid response
+                    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                    } else {
+                        Logger.general.error("Error sending remote log, status code: \((response as? HTTPURLResponse)?.statusCode ?? 0, privacy: .public)")
+                    }
+                } catch {
+                    Logger.general.error("Error sending remote log: \(error, privacy: .public)")
                 }
-                let event = Event(userId: GKLocalPlayer.local.gamePlayerID, eventName: eventName, timestamp: Date())
-                let url = URL(string: "https://hannesnagel.com/api/v2/log")!
-                
-                // Convert Event to JSON data
-                let encoder = JSONEncoder()
-                print(encoder.dateEncodingStrategy)
-                encoder.dateEncodingStrategy = .iso8601
-                let jsonData = try encoder.encode(event)
-                
-                // Create the URLRequest
-                var request = URLRequest(url: url)
-                request.httpMethod = "POST"
-                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.httpBody = jsonData
-                
-                // Perform the request
-                let (_, response) = try await URLSession.shared.data(for: request)
-                
-                // Check for a valid response
-                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
-                } else {
-                    Logger.general.error("Error sending remote log, status code: \((response as? HTTPURLResponse)?.statusCode ?? 0, privacy: .public)")
-                }
-            } catch {
-                Logger.general.error("Error sending remote log: \(error, privacy: .public)")
             }
         }
     }
