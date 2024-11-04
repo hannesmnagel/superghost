0e7ae8efbf4e90464b6dff74f7599db49786f385
Hannes Nagel <hannes.nagel@codeunity.de>
2024-11-04 10:32:16 +0100

    adding os 16 onChange modifier

---
+++ superghost/ContentPlaceHolderView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentPlaceHolderView.swift
+//  superghost
+//
+//  Created by Hannes Nagel on 11/3/24.
+//
+
+import SwiftUI
+
+struct ContentPlaceHolderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentPlaceHolderView()
+}
---
+++ superghost/onChange.swift
@@ -0,0 +1,45 @@
+//
+//  onChange.swift
+//  superghost
+//
+//  Created by Hannes Nagel on 11/3/24.
+//
+
+import SwiftUI
+import Combine
+
+extension View{
+    @_disfavoredOverload
+    @ViewBuilder
+    func onChange<Value: Equatable>(of value: Value, perform action: @escaping (Value, Value) -> Void) -> some View {
+        if #available(iOS 17, tvOS 17, macOS 14, watchOS 7, *) {
+            self.onChange(of: value) { oldValue, newValue in
+                action(oldValue, newValue)
+            }
+        } else {
+            self.modifier(ChangeModifier(value: value, action: action))
+        }
+    }
+}
+//@available(iOS, deprecated: 17)
+private struct ChangeModifier<Value: Equatable>: ViewModifier {
+    let value: Value
+    let action: (Value, Value) -> Void
+
+    @State var oldValue: Value
+
+    init(value: Value, action: @escaping (Value, Value) -> Void) {
+        self.value = value
+        self.action = action
+        _oldValue = .init(initialValue: value)
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .onReceive(Just(value)) { newValue in
+                guard newValue != oldValue else { return }
+                action(oldValue,newValue)
+                oldValue = newValue
+            }
+    }
+}
